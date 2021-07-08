80d6ea83fec84d7ce7c1205409f560f4ef8e22e9
Ninidze1 <giorgi.ninidze.1.@btu.edu.ge>
2021-07-09 00:04:45 +0400

    testcommit

--- app/src/main/java/com/example/meter/MainActivity.kt
+++ app/src/main/java/com/example/meter/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.meter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        d("tagtag", "smth")
     }
 }
