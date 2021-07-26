9162c4b2fe69cc71de887837a76bb2c268f7ce13
giorgiOdishvili <odishvili.giorgi@gmail.com>
2021-07-27 02:49:11 +0400

    saved HERE

--- app/src/main/java/com/example/meter/screens/bottom_nav/community/singlepost/SingleCommunityPostFragment.kt
+++ app/src/main/java/com/example/meter/screens/bottom_nav/community/singlepost/SingleCommunityPostFragment.kt
@@ -19,6 +19,8 @@ import com.example.meter.entity.Comment
 import com.example.meter.entity.community.post.Content
 import com.example.meter.extensions.loadProfileImg
 import com.example.meter.extensions.setGone
+import com.example.meter.extensions.show
+import com.example.meter.extensions.toFormattedDate
 import com.example.meter.network.Resource
 import com.example.meter.repository.firebase.FirebaseRepositoryImpl
 import com.example.meter.utils.transformers.ZoomPageTransformer
@@ -221,9 +223,11 @@ class SingleCommunityPostFragment :
             binding.likeButton.setImageResource(R.drawable.ic_like_unpressed)
 
         }
+        binding.textView4.text = data.createdData.toFormattedDate()
         binding.name.text = data.user.name
         binding.singleTitle.text = data.title
-        binding.descriptionTB.text = data.description     if (data.photoCarUrl.isEmpty()) {
+        binding.descriptionTB.text = data.description
+        if (data.photoCarUrl.isEmpty()) {
             binding.descriptionTB.setGone()
             binding.postWithoutImg.show()
             binding.textWhenNoImg.show()
