e9e74d4bbe9056a9d9ccd3aec41deef85b876083
Ninidze1 <giorgi.ninidze.1.@btu.edu.ge>
2021-07-27 05:08:19 +0400

    post add first page xml added

--- app/src/main/java/com/example/meter/entity/sell/SellCarPost.kt
+++ app/src/main/java/com/example/meter/entity/sell/SellCarPost.kt
@@ -7,18 +7,13 @@ data class SellCarPost(
     val AUX: Boolean,
     val address: String,
     val air_conditioner: Boolean,
-    val airbag: Int,
-    val backuptire: Boolean,
     val bluetooth: Boolean,
     val board_computer: Boolean,
     val car_color: String,
-    val central_lock: Boolean,
     val climate_control: Boolean,
     val cylinder: Int,
     val description: String,
-    val disks: Boolean,
     val door_amount: String,
-    val el_window: Boolean,
     val engine: Double,
     val fuel_type: String,
     val id: Int,
@@ -28,37 +23,19 @@ data class SellCarPost(
     val manufacturer: String,
     val mileage: Int,
     val model: String,
-    val multiwheel: Boolean,
-    val navigation: Boolean,
     val photoUrl: List<String>,
     val price: Int,
     val rear_view_camera: Boolean,
     val releaseYear: String,
-    val signalization: Boolean,
     val start_stop: Boolean,
-    val tire_amount: String,
     val transmission_type: String,
     val user: User?,
     val vin: String,
     val wheel_side: String,
     @SerializedName("კრუიზ_კონტროლი")
     val cruizeControl: Boolean,
-
     @SerializedName("ლუქი")
     val luqi: Boolean,
-
-    @SerializedName("მოცურების_საწინააღმდეგო")
-    val antiSlide: Boolean,
-
-    @SerializedName("სავარძლის_მეხსიერება")
-    val seatMemory: Boolean,
-
-    @SerializedName("სისლე_ფარები")
-    val antiFogLights: Boolean,
-
     @SerializedName("ტექ_დათვალიერება")
     val techInspection: Boolean,
-
-    @SerializedName("ჰიდრავლიკა")
-    val hyravlick: Boolean
 )
